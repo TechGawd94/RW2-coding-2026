033a7cec7b08fd893e1c52d81c226f4bec4757e5
TechGawd94 <sandersjoshua35@yahoo.com>
2026-02-13 03:33:41 +0000

    Created Hello World py script

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+# the hash is a one line comment
+
+"""
+the triple quotes are a multi-line comment
+see
+believ me
+anything inside will not execute
+"""
+
+#this will print to the screen
+print("Hello World!")
