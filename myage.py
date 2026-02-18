2f8e15b8be41d9268f88d3cc63292cd8cc809a2b
TechGawd94 <sandersjoshua35@yahoo.com>
2026-02-18 03:54:50 +0000

    Myname

---
+++ myage.py
@@ -0,0 +1,9 @@
+# birth_year =
+birth_year = 1994
+age = 2026 - birth_year
+name = "Joshua"
+
+print("My name is " + str(name) + " and my age is " + str(age))
+
+#print to the screen:
+#my name is x and my age is y
