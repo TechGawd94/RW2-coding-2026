1ceaf84f1599d49e9b6244914af81948ca043cb8
TechGawd94 <sandersjoshua35@yahoo.com>
2026-02-18 03:02:36 +0000

    a

--- helloworld.py
+++ helloworld.py
@@ -10,8 +10,41 @@ anything inside will not execute
 #this will print to the screen
 print("Hello World!")
 
-name = "Gwen"
-print(name)
+#float
+price = 5.99
+#interger
+age = 35
+#boolean
+is_here = True
+#string
+name = "Joshua"
 
-print(5*5)
+print(type(name))
+print(type(age))
+print(type(is_here))
+print(type(price))
+
+print("Joshua's age is" + str(age))
+
+#this is the basic python if statement
+
+if age > 36:
+    print("you are an unc")
+
+#this a more complex if statement
+"""
+if age > 36:
+    print("you are an unc")
+else:
+    print("you are not an unc")
+    """
+age = 99
+
+    #this an even more complex if statement
+if age > 36:
+    print("you are an unc")
+elif age < 34:
+    print("you are not an unc")
+else:
+    print("you must be 35 years old")
 
