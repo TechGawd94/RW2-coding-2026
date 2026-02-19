eeec45a0d3a28b74a0331c08b17fa1e4e75b3c08
TechGawd94 <sandersjoshua35@yahoo.com>
2026-02-19 02:45:17 +0000

    Functions

--- myage.py
+++ myage.py
@@ -7,3 +7,42 @@ print("My name is " + str(name) + " and my age is " + str(age))
 
 #print to the screen:
 #my name is x and my age is y
+"""
+#welcome to fstrings
+
+print(f"My friend is {name}")
+"""
+"""
+user_name = input("What is your name?")
+num_1 = input("Give me a number ")
+num_2 = input("Give me another number ")
+total = int(num_1) + int(num_2)
+str(total)
+
+print(f"Your name is: {user_name} and the total is {total}")
+
+#print(f"Your name is: {user_name}")
+"""
+
+#lets create functions and learn about global/local variables
+"""
+name = "Josh"
+age =25
+city = "KC"
+
+def my_func_1(city):
+    name = "Brea"
+    age = 21
+    city = "Independence"
+    print(name,age, city)
+
+print(name,age,city)
+my_func_1(city)
+"""
+
+#create variable x with value of 5
+x = 5
+y = "John"
+print(type(x))
+
+
